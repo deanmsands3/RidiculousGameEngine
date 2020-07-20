bfa6061b9a8d59be3c6a221fcfb0e1e22d486a06
Dean M. Sands, III <deanmsands3@gmail.com>
2020-07-20 13:30:36 -0500

    Added SFML Window to Engine. Fleshing out EngineCore

--- src/EngineCore/include/EngineCore.h
+++ src/EngineCore/include/EngineCore.h
@@ -6,15 +6,23 @@
 #define SSS_ENGINECORE_H
 #include "Assets.h"
 #include "TidyWorkers.h"
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
 
 
 class EngineCore: public TidyThread {
+    std::shared_ptr<sf::RenderWindow> _window;
+    sf::Clock _clock;
 protected:
     void setup() override;
     void loop() override;
+    virtual sf::Time tick();
+    virtual bool processEvents();
     void cleanup() override;
 
 public:
+    sf::Clock getClock() const;
+    std::shared_ptr<sf::RenderWindow> getWindow();
     void start() override;
     void stop() override;
     EngineCore();
--- src/EngineCore/src/EngineCore.cpp
+++ src/EngineCore/src/EngineCore.cpp
@@ -4,27 +4,62 @@
 
 #include "../include/EngineCore.h"
 #include "../include/Sandbox.h"
-EngineCore::EngineCore() {
-
-
-}
+EngineCore::EngineCore(): _clock() {}
 
 void EngineCore::start() {
     TidyThread::start();
 }
 
 void EngineCore::setup() {
+    _window=std::make_shared<sf::RenderWindow>(
+        sf::VideoMode(200, 200), "Soldiers of Steam and Steel"
+    );
     Sandbox sandbox;
 }
 
 void EngineCore::cleanup() {
 
 }
+sf::Clock EngineCore::getClock() const {
+    return _clock;
+}
+
+std::shared_ptr<sf::RenderWindow> EngineCore::getWindow(){
+    return _window;
+}
 
 void EngineCore::loop() {
-
+    while (getWindow()->isOpen())
+    {
+        tick();
+    }
 }
 
 void EngineCore::stop() {
+    getWindow()->close();
+}
 
+sf::Time EngineCore::tick() {
+    _clock.restart();
+
+    if(processEvents()){
+        stop();
+        return sf::Time();
+    }
+
+    getWindow()->clear();
+    //_window->draw(shape);
+    getWindow()->display();
+    auto tick_time=_clock.getElapsedTime();
+    return tick_time;
+}
+
+bool EngineCore::processEvents() {
+    sf::Event event;
+    while (getWindow()->pollEvent(event))
+    {
+        if (event.type == sf::Event::Closed)
+            return true;
+    }
+    return false;
 }
