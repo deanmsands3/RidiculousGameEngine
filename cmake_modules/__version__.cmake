9e93fef1a4adb349c8aa71d41ca6705920ccd25d
Dean M. Sands, III <deanmsands3@gmail.com>
2020-08-26 05:56:08 -0500

    More name change stuff. Dropped Boost and Python.

--- cmake_modules/__version__.cmake
+++ cmake_modules/__version__.cmake
@@ -1,7 +1,7 @@
 # Set version information in a config.h file
-set(SSS_VERSION_MAJOR 0)
-set(SSS_VERSION_MINOR 1)
-set(SSS_PATCH 1)
-set(SSS_BUILD 1)
-set(SSS_COMMIT "$Revision: $")
-set(SSS_VERSION ${SSS_VERSION_MAJOR}.${SSS_VERSION_MINOR}.${SSS_PATCH})
+set(RGE_VERSION_MAJOR 0)
+set(RGE_VERSION_MINOR 1)
+set(RGE_PATCH 1)
+set(RGE_BUILD 1)
+set(RGE_COMMIT "$Revision: $")
+set(RGE_VERSION ${RGE_VERSION_MAJOR}.${RGE_VERSION_MINOR}.${RGE_PATCH})
